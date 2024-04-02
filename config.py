309656274d5def1bca1df5274118e8326b81878b
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2024-04-02 10:56:43 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+
+from text_renderer.effect import *
+from text_renderer.corpus import *
+from text_renderer.config import (
+    RenderCfg,
+    NormPerspectiveTransformCfg,
+    GeneratorCfg,
+    SimpleTextColorCfg,
+)
+
+CURRENT_DIR = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+def story_data():
+    return GeneratorCfg(
+        num_image=10,
+        save_dir=CURRENT_DIR / "output",
+        render_cfg=RenderCfg(
+            bg_dir=CURRENT_DIR / "bg",
+            height=32,
+            perspective_transform=NormPerspectiveTransformCfg(20, 20, 1.5),
+            corpus=CharCorpus(
+                CharCorpusCfg(
+                    text_paths=[CURRENT_DIR / "corpus" / "eng_text.txt"],
+                    font_dir=CURRENT_DIR / "font",
+                    font_size=(20, 30),
+                    # num_word=(2, 3),
+                ),
+            ),
+            corpus_effects=Effects(Line(0.9, thickness=(2, 5))),
+            gray=False,
+            text_color_cfg=SimpleTextColorCfg(),
+        ),
+    )
+
+
+configs = [story_data()]
