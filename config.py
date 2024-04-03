25418a16f5446dcee290584727f97be09c23e990
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2024-04-03 11:18:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -20,19 +20,19 @@ def story_data():
         save_dir=CURRENT_DIR / "output",
         render_cfg=RenderCfg(
             bg_dir=CURRENT_DIR / "bg",
-            height=32,
+            height=70,
             perspective_transform=UniformPerspectiveTransformCfg(12, 12, 1.2),
             corpus=CharCorpus(
                 CharCorpusCfg(
                     text_paths=[CURRENT_DIR / "corpus" / "jp_text.txt"],
                     font_dir=CURRENT_DIR / "font",
-                    font_size=(20, 30),
+                    font_size=(66, 68),
                     length=(3,39)
                 ),
             ),
             corpus_effects=Effects(Line(0.2, thickness=(2, 5),line_pos_p=(0, 1, 0, 0, 0, 0, 0, 0, 0, 0))),
             # corpus_effects=Effects(ImgAugEffect(p=0.2,aug=iaa.GaussianBlur(sigma=(0.5, 1.5)))),
-            #corpus_effects=Effects(Padding(p=1, w_ratio=[0.01, 0.05], h_ratio=[0.6, 0.71], center=True)),
+            # corpus_effects=Effects(Padding(p=1, w_ratio=[0.015, 0.021], h_ratio=[0.3, 0.35], center=True)),
             gray=False,
             text_color_cfg=SimpleTextColorCfg(),
         ),
